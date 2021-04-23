24ee30f07a877a2d5d1ba087a6ce2417531eab1e
TheRealKraytonian <kraytonianu@gmail.com>
2021-04-23 09:48:25 -0700

    finished with news project

--- components/Menu.js
+++ components/Menu.js
@@ -1,12 +1,12 @@
 // This is the data we will be using, study it but don't change anything, yet.
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out",
 ];
 
 /* 
@@ -31,3 +31,50 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+const header = document.querySelector(".header");
+function menuMaker(anArray) {
+  const divMain = document.createElement("div");
+  const unorderedList = document.createElement("ul");
+  const listItem0 = document.createElement("li");
+  const listItemOne = document.createElement("li");
+  const listItemTwo = document.createElement("li");
+  const listItemThree = document.createElement("li");
+  const listItemFour = document.createElement("li");
+  const listItemFive = document.createElement("li");
+  const menuButton = document.querySelector(".menu-button");
+
+  divMain.classList.add("menu");
+
+  listItem0.textContent = anArray[0];
+  listItemOne.textContent = anArray[1];
+  listItemTwo.textContent = anArray[2];
+  listItemThree.textContent = anArray[3];
+  listItemFour.textContent = anArray[4];
+  listItemFive.textContent = anArray[5];
+
+  anArray.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.append(item);
+  });
+
+  // for (let i = 0; i < anArray.length; i++) {
+  //   let anArray = document.createElement("li");
+  //   anArray.append(anArray[i]);
+  //   return anArray;
+  // }
+
+  menuButton.addEventListener("click", (event) => {
+    divMain.classList.toggle("menu--open");
+  });
+
+  divMain.appendChild(unorderedList);
+  unorderedList.appendChild(listItem0);
+  unorderedList.appendChild(listItemOne);
+  unorderedList.appendChild(listItemTwo);
+  unorderedList.appendChild(listItemThree);
+  unorderedList.appendChild(listItemFour);
+  unorderedList.appendChild(listItemFive);
+
+  return divMain;
+}
+header.append(menuMaker(menuItems));
